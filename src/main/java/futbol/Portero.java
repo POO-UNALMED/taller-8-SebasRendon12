b8d3c16c5f6f843252aa7650f42ec21b9d5349b0
Sebastian Rendon <sebastian.rendon@quantrack.io>
2020-10-20 19:53:49 -0500

    Fourth try
    Por otro espacio :triumph:

--- src/main/java/futbol/Portero.java
+++ src/main/java/futbol/Portero.java
@@ -27,7 +27,7 @@ public class Portero extends Futbolista {
 	@Override
 	public String toString() {
 		return "El futbolista " + super.getNombre() + " tiene " + super.getEdad() + ", y juega de "
-				+ super.getPosicion() + " con el dorsal " + dorsal + ". Le han marcado" + golesRecibidos;
+				+ super.getPosicion() + " con el dorsal " + dorsal + ". Le han marcado " + golesRecibidos;
 	}
 
 }
